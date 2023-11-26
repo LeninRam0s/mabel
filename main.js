5a6ecac50047957b2bbed837338ca0e8e669992b
LeninRam0s <djalexander.rmix@gmail.com>
2023-11-26 14:40:26 -0600

    Update

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+
+
+setTimeout(function() {
+    document.querySelector('p').classList.add('active');
+  }, 5000); // 5000 milisegundos (5 segundos)
